c8bf3613c7111f1d9ca92533a6632c4e444b48df
hehongan <hehongan@h-partners.com>
2026-03-16 15:43:42 +0800

    Implements Trap on A5.
    Co-authored-by: hehongan<hehongan@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !909 merge master into master
    
    Implements Trap on A5.
    
    Created-by: hehongan
    Commit-by: hehongan
    Merged-by: cann-robot
    Description: ## 描述
    Implements Trap on A5.
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #xxx-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] 🐛 fix: Bug 修复
    - [ ] ✨ feat: 新功能
    - [ ] ⚡ perf: 性能优化
    - [ ] ♻️ refactor: 代码重构
    - [ ] 🧪 test: 新增或修改测试
    - [ ] 📝 docs: 文档更新
    - [ ] 🔧 ci: CI/CD 配置修改
    - [ ] ↩️ revert: 回退
    - [ ] 🧹 chore: 其他，请具体描述
    
    
    See merge request: cann/asc-devkit!909

--- impl/basic_api/dav_c310/kernel_operator_sys_var_impl.h
+++ impl/basic_api/dav_c310/kernel_operator_sys_var_impl.h
@@ -1,4 +1,4 @@
-/**
+﻿/**
 * Copyright (c) 2025 Huawei Technologies Co., Ltd.
 * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
 * CANN Open Software License Agreement Version 2.0 (the "License").
@@ -158,7 +158,7 @@ __aicore__ inline void SetPcieWRCtrlImpl(bool isSetPcie, uint8_t maxBurstLen)
 
 __aicore__ inline void TrapImpl()
 {
-    ASCENDC_REPORT_NOT_SUPPORT(false, "Trap");
+    trap();
 }
 } // namespace AscendC
 #endif // ASCENDC_MODULE_OPERATOR_SYS_VAR_IMPL_H
