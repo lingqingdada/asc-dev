4e02e54ef153d827795637909bb7b99968f315d3
ning_x <chenning54@huawei.com>
2026-04-02 19:41:38 +0800

    清理tests中已失效的环境变量
    Co-authored-by: ning_x<chenning54@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1259 merge fix_issue into master
    
    清理tests中已失效的环境变量
    
    Created-by: ning_x
    Commit-by: ning_x
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    当前tests环境配置中存在遗留的已失效变量
    当前pr对其进行清理
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #xxx-->
    issue [#319](https://gitcode.com/cann/asc-devkit/issues/319) 
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] 🐛 fix: Bug 修复
    - [ ] ✨ feat: 新功能
    - [ ] ⚡ perf: 性能优化
    - [x] ♻️ refactor: 代码重构
    - [x] 🧪 test: 新增或修改测试
    - [ ] 📝 docs: 文档更新
    - [x] 🔧 ci: CI/CD 配置修改
    - [ ] ↩️ revert: 回退
    - [ ] 🧹 chore: 其他，请具体描述
    
    
    See merge request: cann/asc-devkit!1259

--- tests/python/ci/set_python_env.sh
+++ tests/python/ci/set_python_env.sh
@@ -13,32 +13,9 @@ set -e
 
 top_dir=$1
 test_type=$2
-soc_info=$3
 base_path=$top_dir/build/
 
-#PYTHONPATH
-export PYTHONPATH=$top_dir/open_source/tvm/python:$top_dir/open_source/tvm/topi/python:$PYTHONPATH
-
-#PATH
-export PATH=$top_dir/build/bin/toolchain/x86/ubuntu/ccec_libs/ccec_x86_ubuntu_20_04_adk/bin:$PATH
-
 #LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$base_path/llt/tensor_engine/ut:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$base_path/metadef/register:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$base_path/air/graph_metadef/register:$LD_LIBRARY_PATH
 export ASCEND_OPP_PATH=$top_dir/out/onetrack/llt/$test_type/obj
 mkdir -p "$ASCEND_OPP_PATH"
 echo "os=linux" > "$ASCEND_OPP_PATH/scene.info"
-
-
-# fast execute
-# top_dir=~/davinci_trunk
-# test_type=py_st # py_ut
-# base_path=$top_dir/tmp/llt_gccnative-prefix/src/llt_gccnative-build/
-
-# export PATH=$top_dir/build/bin/toolchain/x86/ubuntu/ccec_libs/ccec_x86_ubuntu_20_04_adk/bin:$PATH
-# export PYTHONPATH=$top_dir/open_source/tvm/python:$top_dir/open_source/tvm/topi/python:$PYTHONPATH
-# export LD_LIBRARY_PATH=$base_path/llt/tensor_engine/ut:$LD_LIBRARY_PATH
-# export LD_LIBRARY_PATH=$base_path/metadef/register:$LD_LIBRARY_PATH
-# export ASCEND_OPP_PATH=$top_dir/out/onetrack/llt/$test_type/obj
-
