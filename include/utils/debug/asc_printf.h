3bdb6f6e11b4c67041eb2769ad538445d68c08c1
liuliu876 <liuxin429@huawei.com>
2026-04-09 14:52:42 +0800

    delete declaration of print_impl in asc_printf.h
    Co-authored-by: liuliu876<liuxin429@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1476 merge delete_unused_apis into master
    
    delete declaration of print_impl in asc_printf.h
    
    Created-by: liuliu876
    Commit-by: liuliu876
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    CI告警printf_impl(__gm__ const char* fmt, Args&&... args)在接口文件中，不在接口资料中，实际也非对外接口，删除该函数在头文件中的声明。
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #xxx-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] 🐛 fix: Bug 修复
    - [ ] ✨ feat: 新功能
    - [ ] ⚡ perf: 性能优化
    - [ ] ♻️ refactor: 代码重构
    - [ ] 🧪 test: 新增或修改测试
    - [ ] 📝 docs: 文档更新
    - [ ] 🔧 ci: CI/CD 配置修改
    - [ ] ↩️ revert: 回退
    - [ ] 🧹 chore: 其他，请具体描述
    
    
    See merge request: cann/asc-devkit!1476

--- include/utils/debug/asc_printf.h
+++ include/utils/debug/asc_printf.h
@@ -42,19 +42,16 @@ static __attribute__((noinline)) __SIMT_DEVICE_FUNCTIONS_DECL__ void printf(cons
 
 #ifndef __NPU_COMPILER_INTERNAL_PURE_SIMT__
 #ifndef __CHECK_FEATURE_AT_PRECOMPILE
+#if (__NPU_ARCH__ == 2002) || (__NPU_ARCH__ == 2201) || (__NPU_ARCH__ == 3510)
+#include "impl/utils/debug/asc_aicore_printf_impl.h"
+#endif
 namespace __asc_aicore {
-template <class... Args>
-__aicore__ inline void printf_impl(__gm__ const char* fmt, Args&&... args);
 template <class... Args>
 __aicore__ inline void printf(__gm__ const char* fmt, Args&&... args)
 {
     printf_impl(fmt, args...);
 }
 } // namespace __asc_aicore
-#endif
-
-#if (__NPU_ARCH__ == 2002) || (__NPU_ARCH__ == 2201) || (__NPU_ARCH__ == 3510)
-#include "impl/utils/debug/asc_aicore_printf_impl.h"
 #endif
 #endif
 
